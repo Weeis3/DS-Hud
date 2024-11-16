9434483c07c3faf18a9a69727381adc7289e7332
Weeis <142181048+Weeis3@users.noreply.github.com>
2024-11-16 21:03:17 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Weeis'
+description 'HUD for QBcore'
+version '1.0.0'
+
+client_scripts {
+    'client/client.lua',
+}
+
+server_scripts {
+    'server/server.lua',
+}
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/fonts/HighSpeed.ttf',  -- The custom font file
+    'html/script.js',
+}
+
+ui_page 'html/index.html'
