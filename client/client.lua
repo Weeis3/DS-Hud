4f50dac05d346d032f6f4c400ba283d7accc466b
Weeis <142181048+Weeis3@users.noreply.github.com>
2025-01-30 23:04:00 +0200

    Update client.lua
    Added ESX Support

--- client/client.lua
+++ client/client.lua
@@ -1,12 +1,41 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = nil
+local ESX = nil
+local cash = nil
+local bank = nil
 CreateThread(function()
+    if GetResourceState('qb-core') == 'started' then
+        QBCore = exports['qb-core']:GetCoreObject()
+    elseif GetResourceState('es_extended') == 'started' then
+        ESX = exports['es_extended']:getSharedObject()
+    else
+        print("Error: ESX or QB is not running please check framework!")
+        return
+    end
+
     while true do
         Wait(1000)
-        local playerData = QBCore.Functions.GetPlayerData()
-        if playerData and playerData.money then
-            local cash = playerData.money["cash"] or 0
-            local bank = playerData.money["bank"] or 0
-            print("Cash: " .. cash .. " Bank: " .. bank)
+        local playerData = nil
+        if QBCore then
+            playerData = QBCore.Functions.GetPlayerData()
+        elseif ESX then
+            ESX.TriggerServerCallback('esx:getPlayerData', function(data)
+                playerData = data
+            end)
+            Wait(500)
+        end
+
+        if playerData and QBCore then
+            cash = playerData.money["cash"] or 0
+            bank = playerData.money["bank"] or 0
+            SendNUIMessage({
+                action = "updateMoney",
+                cash = cash,
+                bank = bank
+            })
+        elseif playerData and ESX then
+            TriggerServerEvent('ds-hud:getPlayerMoney', "bank")
+            TriggerServerEvent('ds-hud:getPlayerMoney', "money")
+            
             SendNUIMessage({
                 action = "updateMoney",
                 cash = cash,
@@ -15,8 +44,20 @@ CreateThread(function()
         else
             print("Error: Player data not found!")
         end
-		TriggerServerEvent('ds-hud:getJobData')
+        TriggerServerEvent('ds-hud:getJobData')
     end
+end)
+RegisterNetEvent('ds-hud:receiveMoney')
+AddEventHandler('ds-hud:receiveMoney', function(account, money)
+
+    if account == "bank" then
+        bank = money
+    elseif account == "money" then
+        cash = money
+    else
+        print("Not a valid account")
+    end
+
 end)
 RegisterNetEvent('ds-hud:updateJobData')
 AddEventHandler('ds-hud:updateJobData', function(job, grade)
